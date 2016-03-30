fcb23daa51e31a5ea9300d84792c0fad191c78c4
em <michael@joinbox.com>
2016-03-30 17:43:40 +0200

    client server communication works

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 
     require('ee-log');
 
-    let Cli = require('./lib/Cli');
-    let Server = require('./lib/Server');
-    let Client = require('./lib/Client');
+    const Cli = require('./lib/Cli');
+    const Server = require('./lib/Server');
+    const Client = require('./lib/Client');
+    const Report = require('./lib/Report');
 
     
-    let cli = new Cli();
+    const cli = new Cli();
 
     if (cli.mode === 'server') new Server(cli);
     else if (cli.mode === 'client') new Client(cli);
-    else cli.warn(`Cannot determine in which mode to run, please specifiy the mode in the config file using the 'mode' property and either 'client' or 'server' as value!`).exit(1);
+    else if (cli.mode === 'report') new Report(cli);
+    else cli.warn(`Cannot determine in which mode to run, please specifiy the mode in the config file using the 'mode' property and either 'client', 'report' or 'server' as value!`).exit(1);
 
 })();
